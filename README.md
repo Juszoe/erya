5b0a49b24db53f8ce96a287962ef1550f5bb1442
Juszoe <Juszoe@outlook.com>
2019-04-03 15:08:46 +0800

    v0.9

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-# 尔雅助手
-### 后台播放
-### 划关键字搜答案，部分题目可能搜不到
-### 没有其他更多的功能，无任何风险，这不是一个全自动脚本，只是解放你的电脑，更自由的学习
-<div
-    style="cursor:move;height:60px;width:300px;background-color:#71AAFF;text-align:center;color:#fff;border-radius:5px;font-size:20px;line-height:60px;position: relative;">
+# 简介
+- 本质是一个脚本
+- 尔雅助手，轻松后台播放、搜题，学习无忧
+- 无需下载安装，点击即用
+- 这不是全自动脚本，选择课程和答题仍需要手动，无风险
+- 仅支持PC端现代浏览器（Chrome，Firefox，等等）
+- 自动使用最新版脚本，无需手动更新
+# 功能
+- 解除课程视频播放限制，视频可后台播放
+- 课程播放完毕或暂停会弹出提示
+- 划关键字搜答案
+- 共享答案，丰富题库
+# 使用方法
+1. 拖动下方按钮到浏览器的书签栏(也称收藏夹)，相当于添加一个网页小书签，但书签的网址项里填的是一串脚本。
+    <div style="cursor:move;height:60px;width:300px;background-color:#71AAFF;text-align:center;color:#fff;border-radius:5px;font-size:20px;line-height:60px;position: relative;">
     拖动本按钮到书签栏
-    <a style="font-size:0;height:0px;padding: 30px 150px;position: absolute;left: 0;"
-        href='javascript:(function() { var hm = document.createElement("script"); hm.src = "https://juszoe.github.io/erya/helper.js"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })();'
-        onclick="alert('请把按钮拖动到书签栏');return false;">尔雅助手</a>
-</div>
+        <a style="font-size:0;height:0px;padding: 30px 150px;position: absolute;left: 0;"
+            href='javascript:(function() { var hm = document.createElement("script"); hm.src = "https://juszoe.github.io/erya/helper.js"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })();'
+            onclick="alert('请把按钮拖动到书签栏');return false;">尔雅助手</a>
+    </div>
+2. 打开[尔雅学习界面](https://mooc1-1.chaoxing.com/mycourse/studentstudy)
+3. 点击一下书签栏里的“尔雅助手”，如下所示说明脚本运行中
+4. 划词搜索答案，选中关键词自动搜索（题库有限，部分题目可能搜不到，请不要频繁尝试）
+# 其他
+- 有更加强大的脚本，但正由于太过强大会被检测到，所以并不推荐 ~~平平淡淡才是真~~<br>
+以下为第三方脚本
+    - [超星网课助手](https://greasyfork.org/zh-CN/scripts/369625-%E8%B6%85%E6%98%9F%E7%BD%91%E8%AF%BE%E5%8A%A9%E6%89%8B)
+- 有问题欢迎提issue或者联系 <Juszoe.cn+erya@gmail.com>
+- 如果觉得这个脚本有用欢迎推荐给你的同学
--- helper.js
+++ helper.js
@@ -1,10 +1,10 @@
-(function () {
+﻿(function () {
     // 检测url
     if (!window.location.href.startsWith('https://mooc1-1.chaoxing.com/mycourse/studentstudy')) {
         alert('请在"学生学习页面"使用！ 地址为 https://mooc1-1.chaoxing.com/mycourse/studentstudy');
         return;
     };
-    // if (window._eryahelper) return;
+    if (window._eryahelper) return;
     window._eryahelper = true;
 
     // 提醒接口
@@ -14,8 +14,8 @@
         } else if (Notification.permission === "granted") {
             return function (msg) {
                 var notification = new Notification('尔雅助手', {
-                    body: msg + '\n点击关闭',
-                    icon: 'https://github.com/favicon.ico'
+                    body: msg + '\n点击关闭\n',
+                    icon: 'https://juszoe.github.io/erya/favicon.ico'
                 });
                 notification.onclick = function () {
                     notification.close();
@@ -36,66 +36,197 @@
                 }
             });
         }
-        $('#mainid > h1').append('<p style="color: red;">显示通知功能需要权限，请允许通知</p>');
+        $('#helper').append('<p style="color: red;">显示通知功能需要权限，请允许通知</p>');
         return alert;
     })();
 
     // 使用img进行跨域请求
-    function postAnswer(question, answer, correct) {
-        var img = document.createElement("img");
-        img.src = "http://localhost:8700/answer?question=" + question + "&answer=" + answer + "&correct=" + correct;
-        img.hidden = true;
-        document.querySelector("body").appendChild(img);
+    function postAnswer(answers) {
+        $.ajax({
+            url: "https://api.tensor-flow.club:8700/collect",
+            type: "GET",
+            data: {
+                answers: JSON.stringify(answers)
+            },
+            dataType: "jsonp", //指定服务器返回的数据类型
+            success: function (data) {
+                console.log(data);
+            }
+        });
     }
+    function getAnswer(course, keyword, cb) {
+        $.ajax({
+            url: "https://api.tensor-flow.club:8700/answer",
+            type: "GET",
+            data: {
+                course: course,
+                keyword: keyword
+            },
+            dataType: "jsonp", //指定服务器返回的数据类型
+            success: cb
+        });
+    }
+
+    // 初始化
+    var course = $('#mainid > h1').text()
+    $('.content').prepend('<h1 id="helper" style="text-align:center;font-size:28px;">尔雅助手</h1>');
+    if (window.frames['iframe'].contentDocument.readyState == 'complete') {
+        start();
+    }
+    $('iframe').on("load", function () {
+        start();
+    })
 
     // 启动助手
     function start() {
+        // 解除视频限制
+        var trycount = 0;
         (function videoListener() {
             setTimeout(function () {
                 try {
+                    trycount++;
                     var video = window.frames['iframe'].contentDocument.querySelector('iframe').contentDocument.querySelector('video')
                     video.addEventListener('ended', function () {
-                        notify('课程视频播放完毕');
+                        notify('《' + course + '》 课程视频播放完毕');
                     });
                     video.addEventListener('pause', function () {
                         setTimeout(function () {
-                            if (video.paused)
-                                notify('课程视频已暂停');
-                        }, 8000);
+                            if (video.paused && !video.ended)
+                                video.play();
+                        }, 100);
                     });
                 } catch (e) {
+                    if (trycount > 20) return;
                     videoListener()
                 }
             }, 1000);
         })();
 
-        
-        
-        
-    }
-    $('.content').prepend('<h1 style="text-align:center;font-size:28px;">尔雅助手</h1>');
-    $('iframe').on("load", function () {
-        console.log(1111)
-        start();
         // 共享答案
         try {
             // 嵌套太多iframe了吧...
             var timu = window.frames['iframe'].contentDocument.querySelector('iframe').contentDocument.querySelector('iframe').contentDocument.querySelectorAll('.TiMu');
+            var result = [];
             $(timu).each(function () {
                 var question = $(this).find('.Zy_TItle .clearfix').text().trim();
                 var answer = $(this).find('.Py_answer span')[0].innerText.trim().replace('我的答案：', '');
                 var correct = $(this).find('.fr').hasClass("dui");
-                postAnswer(question, answer, correct);
+                result.push({
+                    course: course,
+                    question: question,
+                    answer: answer,
+                    correct: correct
+                });
             });
+            postAnswer(result);
         }
         catch (e) { }
-    })
 
-    
-    // 如果切换课程，重新启动助手
-    // $('.ncells a').unbind("click").on('click', function(){
-    //     console.log(111)
-        
-    // })
+        // 划词助手
+        try {
+            var idocument = window.frames['iframe'].contentDocument.querySelector('iframe').contentDocument.querySelector('iframe').contentDocument;
+            var iwindow = window.frames['iframe'].contentDocument.querySelector('iframe').contentDocument.querySelector('iframe').contentWindow;
+            $(idocument.querySelector('body')).prepend('<div id="answer" style="border-radius:5px;font-size:16px;background-color:#71AAFF;color:#fff;padding: 5px;">答案区<div>');
+            idocument.addEventListener('mouseup', function (e) {
+                var text = iwindow.getSelection().toString().trim();
+                var $answer = $(idocument.querySelector('#answer'));
+                if (text) {
+                    $answer.text('正在搜索答案中...');
+                    getAnswer(course, text, function (data) {
+                        $answer.text('');
+                        if(data.length == 0) {
+                            $answer.text('未搜索到答案');
+                        }
+                        for (var i = 0; i < data.length; i++) {
+                            var o = data[i]
+                            $answer.append('<p>【题目】 ' + o.question + '</p>');
+                            $answer.append('<p>【答案】 ' + o.answer + '</p>');
+                            $answer.append('<hr style="border:none;border-top: 1px solid #fff;">');
+                        }
+                    })
+                }
+            });
+        } catch (e) { console.log(e) }
+
+    }
+
+    // 修改原函数
+    window.getTeacherAjax = function (courseId, clazzid, chapterId, cpi, chapterVerCode) {
+        closeChapterVerificationCode();
+        if (courseId == 0 || clazzid == 0 || chapterId == 0) {
+            alert("无效的参数！");
+            return;
+        }
+        if (typeof (cpi) == 'undefined') {
+            cpi = 0;
+        }
+        document.getElementById("mainid").innerHTML = "<div style=\"width:32px;height:32px;margin:0 auto;padding:300px 0\"><img src=\"/images/courselist/loading.gif\" /></div>"
+        jQuery.post("/mycourse/studentstudyAjax",
+            {
+                courseId: courseId
+                , clazzid: clazzid
+                , chapterId: chapterId
+                , cpi: cpi
+                , verificationcode: chapterVerCode || ''
+            },
+            function (data) {
+                data = data.replace(/(^\s*)|(\s*$)/g, "");
+                var doc = document.getElementById("mainid");
+                jQuery(doc).html(data);
+                $('iframe').on("load", function () {  // 修改原函数，在这里添加了事件
+                    start();
+                })
+                if (data.indexOf('showChapterVerificationCode') > -1) {
+                    recordCheckedChapterParam(courseId, clazzid, chapterId, cpi);
+                    return;
+                }
+                document.getElementById("iframe").src = "/knowledge/cards?clazzid=" + clazzid + "&courseid=" + courseId + "&knowledgeid=" + chapterId + "&num=0&ut=s&cpi=55761320&v=20160407-1";
+                var el = $('#iframe');
+                //var openlockdiv=document.getElementById("openlock");
+                if ($("#openlock").length > 0) {
+                    var count = document.getElementById("cardcount").value;
+                    if (count == 1) {
+                        setTimeout('openlockshow();', 2000);
+                    }
+                }
+                if ($("#cur" + chapterId + " .orange01").length > 0) {
+
+                    jQuery.ajax({
+                        type: "get",
+                        url: "/edit/validatejobcount",
+                        data: {
+                            courseId: courseId
+                            , clazzid: clazzid
+                            , nodeid: chapterId
+                        },
+                    });
+                }
+                window.ed_reinitIframe = function ed_reinitIframe() {
+                    var iframe = el[0];
+
+                    try {
+                        var bHeight = iframe.contentWindow.document.body.scrollHeight;
+                        var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+                        var height = Math.max(bHeight, dHeight);
+                        el.attr('height', height);
+                    } catch (ex) { }
+                }
+                window.setInterval("ed_reinitIframe()", 200);
+
+                var tab = 0;
+                if (tab == 3) {
+                    getClazzNote(); changePan('3');
+                } else if (tab == 2) {
+                    getChapterRightDiscuss(); changePan('2');
+                } else {
+                    changePan('1');
+                }
+            }
+
+        );
+        window.setInterval("setposition()", 200);
+        jobflagOperation();
+        scroll(0, 0);
+    }
 
 })()
